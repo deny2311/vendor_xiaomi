6b8c8abf0715bc4c6aca842508627286948ab783
Huexxx <huexxx@gmail.com>
2022-02-24 19:00:42 +0100

    msm8998-common: Drop QTI datastatusnotification service  * its useless and references some functions that are no longer    present in android 12, thus crashing ril with dual sim

--- msm8998-common/msm8998-common-vendor.mk
+++ msm8998-common/msm8998-common-vendor.mk
@@ -611,7 +611,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
