f161cc688865bc90980d62e219e3630ecfb1c0c4
daniml3 <daniel@danielml.dev>
2022-05-21 06:53:19 +0000

    msm8998-common: Drop QTI datastatusnotification * In some situations this app crashes due to a missing framework method on A12,   causing network fluctuation with dual sim.
    * As it isn't needed, let's just remove it
    
    Change-Id: I7649956d630659f81094f66a6fef334511959aef

--- msm8998-common/msm8998-common-vendor.mk
+++ msm8998-common/msm8998-common-vendor.mk
@@ -650,7 +650,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
